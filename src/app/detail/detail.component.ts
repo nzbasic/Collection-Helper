bad0170d7918b863e32c7e83cab50d4f0ce38089
nzbasic <jcop589@aucklanduni.ac.nz>
2021-07-03 14:53:55 +1200

    init

---
+++ src/app/detail/detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html'
+})
+export class DetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('DetailComponent INIT');
+   }
+
+}
