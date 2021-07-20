82dab33d55785c1214dc95b24892d687265e3ac2
nzbasic <jcop589@aucklanduni.ac.nz>
2021-07-20 17:30:11 +1200

    add routes for import export

--- app/router/collections.ts
+++ app/router/collections.ts
@@ -1,6 +1,10 @@
+import { dialog } from "electron";
 import * as express from "express";
 import { addCollection, addMaps, mergeCollections, removeCollections, removeMaps, renameCollection } from "../util/collections";
 import { collections } from "../util/parsing/collections";
+import { win } from '../main'
+import { exportCollection, importCollection, percentage } from "../util/database/collection";
+import { beatmapMap } from "../util/parsing/cache";
 
 const router = express.Router();
 
@@ -29,15 +33,55 @@ router.route("/remove").post(async (req, res) => {
 })
 
 router.route("/addMaps").post(async (req, res) => {
-  let body: { name: string, hashes: string[] } = req.body
+  const body: { name: string, hashes: string[] } = req.body
   await addMaps(body.name, body.hashes)
   res.json(collections)
 })
 
 router.route("/removeMaps").post(async (req, res) => {
-  let body: { name: string, hashes: string[] } = req.body
+  const body: { name: string, hashes: string[] } = req.body
   await removeMaps(body.name, body.hashes)
   res.json(collections)
+})
+
+router.route("/export").post(async (req, res) => {
+
+  let fileName: string = req.body.name
+  fileName = fileName.replace(/[<>:"/\\|?*]/g, '_')
+
+  const dialogRes = await dialog.showSaveDialog(win, {defaultPath: fileName, filters: [{ name: 'Collection Database File', extensions: ['db']}]})
+  if (!dialogRes.canceled) {
+    await exportCollection(req.body.name, req.body.exportBeatmaps, dialogRes.filePath)
+  }
+
+  res.json(dialogRes)
+})
+
+router.route("/import").post(async (req, res) => {
+  const dialogRes = await dialog.showOpenDialog(win, {filters: [{ name: 'Collection Database File', extensions: ['db']}]})
+  if (!dialogRes.canceled) {
+    await importCollection(dialogRes.filePaths[0], req.body.name)
+  }
+
+  res.json(collections)
+})
+
+router.route("/setCount").post((req, res) => {
+  const body: { hashes: string[] } = req.body
+  const setSet = new Set<number>()
+
+  body.hashes.forEach(hash => {
+    const beatmap = beatmapMap.get(hash)
+    if (beatmap) {
+      setSet.add(beatmap.setId)
+    }
+  })
+
+  res.json(setSet.size)
+})
+
+router.route("/progress").get((req, res) => {
+  res.json(percentage)
 })
 
 export default router
