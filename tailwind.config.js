adc34fe4a77825f46858a6ffc47eac4ab32599f5
nzbasic <jcop589@aucklanduni.ac.nz>
2021-07-22 15:16:08 +1200

    include tailwind config in repo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        one: "#194350",
+        two: "#d5dde0",
+        three: "#0a2b36",
+        default: "#E5E7EB",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
